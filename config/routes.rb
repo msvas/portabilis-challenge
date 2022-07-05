67c2aef3562ffd552bbcb7eedf189d5ce80a4622
Marcelo <msouvas@gmail.com>
2022-07-05 17:21:46 -0300

    adiciona rotas do admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
+  namespace :api do
+    namespace :v1 do
+
+      namespace :admin do
+        resources :users, only: [:destroy] do
+          get 'suspend', to: 'users#suspend', as: 'suspend'
+        end
+      end
+
+    end
+  end
 end
