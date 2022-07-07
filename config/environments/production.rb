bc16f728753f22f790e7fffb39d8f7db32e521f7
Marcelo <msouvas@gmail.com>
2022-07-07 13:21:20 -0300

    adiciona mailgun em produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Settings to send emails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'challenge-marcelo-backend.heroku.com',
+    :authentication => :plain,
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
