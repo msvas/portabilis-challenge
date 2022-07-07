bb261809e6235bf626855e4ca876d253ba1fbd43
Marcelo <msouvas@gmail.com>
2022-07-07 15:08:40 -0300

    muda configuração de email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,14 +73,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Settings to send emails
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'challenge-marcelo-backend.heroku.com',
-    :authentication => :plain,
+  config.action_mailer.default_url_options = { :host => 'https://challenge-marcelo-backend.heroku.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["GMAIL_USER"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => :plain,
+    :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
