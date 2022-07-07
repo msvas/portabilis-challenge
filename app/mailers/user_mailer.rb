34904ee304007092b1d80af437bbd9e86a358206
Marcelo <msouvas@gmail.com>
2022-07-07 15:29:00 -0300

    adiciona from nos emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < Devise::Mailer
+  default from: "msouvas@gmail.com"
 
   def welcome(record, opts={})
     @user = record
     devise_mail(record, :welcome, opts)
   end
-  
+
 end
